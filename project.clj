98f5959b57ec8bdaff439b18ffeadda4173102d9
kokopelli314 <nacclonts@gmail.com>
2018-10-27 11:25:16 -0600

    add github PR feed"

--- project.clj
+++ project.clj
@@ -5,7 +5,10 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
-                 [hiccup "1.0.5"]]
+								 [hiccup "1.0.5"]
+								 [clj-http "3.9.1"]
+								 [cheshire "5.8.1"]
+								 [markdown-clj "1.0.5"]]
   :plugins [[lein-ring "0.12.4"]]
   :ring {:handler activity-feed.handler/app}
   :profiles
--- src/activity_feed/views.clj
+++ src/activity_feed/views.clj
@@ -1,7 +1,29 @@
 (ns activity-feed.views
   (:require [clojure.string :as str]
             [hiccup.page :as page]
-            [ring.util.anti-forgery :as util]))
+						[ring.util.anti-forgery :as util]
+						[clj-http.client :as client]
+						[cheshire.core :as json]
+						[markdown.core :refer [md-to-html-string]]))
+
+
+(defn get-pull-request-data []
+	"
+	Returns vector of items containing PR data.
+	"
+	(let [response (client/get
+									"https://api.github.com/search/issues?q=author%3Akokopelli314+type%3Apr&sort=updated"
+									{:accept :json})]
+		(get (json/parse-string (get response :body)) "items")))
+
+(defn make-pull-request-html [pr]
+	[:div
+		[:h3 (get pr "title")]
+		[:p (md-to-html-string (get pr "body"))]
+		[:p (get pr "comments")]])
+
+(defn section-pull-requests [prs]
+	(map make-pull-request-html prs))
 
 
 (defn gen-page-head
@@ -15,5 +37,10 @@
   (page/html5
     (gen-page-head "Bonkers")
     [:h1 "Open Source"]
-    [:p "Latest activity from GitHub."]
-    [:img {:src "https://avatars0.githubusercontent.com/u/10605105" :width "200px"}]))
+    [:p "Latest activity from GitHub"]
+		[:img
+			{:src "https://avatars0.githubusercontent.com/u/10605105"
+			 :width "200px"
+			 :class "profile-image"}]
+		[:div
+			[:p (section-pull-requests (get-pull-request-data))]]))
